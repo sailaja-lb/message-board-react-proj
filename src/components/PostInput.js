216b726d0726008e9476d1861d469e16c13eaf17
sailaja-lb <lakkakulasailaja@gmail.com>
2022-05-09 15:43:56 -0500

    home

---
+++ src/components/PostInput.js
@@ -0,0 +1,34 @@
+import {Button, Card, Col, Form, Row} from "react-bootstrap";
+
+export default function PostInput({post, onPostChange, onCancel, onApply}) {
+    const { date, message } = post
+
+    return <Card>
+        <Card.Header>
+            <Row className='align-items-center'>
+                <Col xs='auto'>
+                    <Button title='Apply' variant={"outline-success"} size='sm'
+                            onClick={onApply}>
+                        Apply
+                    </Button>
+                </Col>
+                <Col xs='auto'>
+                    <Button title='Cancel' variant={"outline-danger"} size='sm'
+                            onClick={onCancel}>
+                        Cancel
+                    </Button>
+                </Col>
+                <Col xs='auto'>
+                    <Form.Control type='date' placeholder='Date'
+                                  defaultValue={date.toISOString().substring(0, 10)}
+                                  onChange={e => onPostChange({...post, date: new Date(e.target.value)})}/>
+                </Col>
+            </Row>
+        </Card.Header>
+        <Card.Body>
+            <Form.Control as="textarea" rows={2} defaultValue={message}
+                          placeholder='Post message'
+                          onChange={e => onPostChange({...post, message: e.target.value})}/>
+        </Card.Body>
+    </Card>
+}
---
+++ src/components/PostInput.test.js
@@ -0,0 +1,85 @@
+import {render, screen} from "@testing-library/react";
+import PostInput from "./PostInput";
+import userEvent from "@testing-library/user-event";
+
+test('should show input fields with current post data', () => {
+    let post = {
+        date: new Date(),
+        message: 'Message1',
+        user: ''
+    }
+
+    const setPost = jest.fn()
+    const cancel = jest.fn()
+    const apply = jest.fn()
+
+    const {rerender} = render(<PostInput post={post} onPostChange={setPost} onCancel={cancel}
+                                           onApply={apply}/>)
+
+    expect(screen.getByDisplayValue(post.message)).toBeInTheDocument()
+    expect(screen.getByDisplayValue(post.date.toISOString().substring(0,10))).toBeInTheDocument()
+    expect(screen.getByTitle('Apply')).toBeInTheDocument()
+    expect(screen.getByTitle('Cancel')).toBeInTheDocument()
+    rerender(<PostInput post={post} setPost={setPost} cancel={cancel}/>)
+})
+
+it('should call onCancel when cancel button clicked', () => {
+    let post = {
+        date: new Date(),
+        message: 'Message1',
+        user: ''
+    }
+
+    const setPost = jest.fn()
+    const cancel = jest.fn()
+    const apply = jest.fn()
+
+    render(<PostInput post={post} onPostChange={setPost} onCancel={cancel}
+                        onApply={apply}/>)
+    screen.getByTitle('Cancel').click()
+    expect(cancel).toHaveBeenCalled()
+})
+
+it('should call onApply when the apply button is clicked', () => {
+    let post = {
+        date: new Date(),
+        message: 'Message1',
+        user: ''
+    }
+    const setPost = jest.fn()
+    const cancel = jest.fn()
+    const apply = jest.fn()
+
+    render(<PostInput post={post} onPostChange={setPost} onCancel={cancel}
+                        onApply={apply}/>)
+    screen.getByTitle('Apply').click()
+    expect(apply).toHaveBeenCalled()
+})
+
+it('should dispatch EDIT_POST when any field changes', () => {
+    const postToEdit = {
+        date: new Date(),
+        message: 'Message1',
+        user: ''
+    }
+
+    const setPost = jest.fn()
+    const cancel = jest.fn()
+    const apply = jest.fn()
+
+    render(<PostInput post={postToEdit} onPostChange={setPost} onCancel={cancel}
+                        onApply={apply}/>)
+    const titleElement = screen.getByDisplayValue(postToEdit.message)
+    const dateElement = screen.getByDisplayValue(postToEdit.date.toISOString().substring(0,10))
+    const newText = 'A'
+    userEvent.type(titleElement, newText)
+    let post = {...postToEdit, message: postToEdit.message + newText}
+    expect(setPost).toHaveBeenCalledWith(post)
+
+    const yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - 1)
+    yesterday.setTime(0)
+    userEvent.type(dateElement, yesterday.toISOString().substring(0, 10))
+    post = {...postToEdit, date: yesterday}
+    expect(setPost).toHaveBeenCalledWith(post)
+})
