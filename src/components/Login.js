1bf927cf60b3ed0ac2d6ccb2f1713b4966c36f93
sailaja-lb <lakkakulasailaja@gmail.com>
2022-05-09 16:37:52 -0500

    home

---
+++ src/components/Login.js
@@ -0,0 +1,74 @@
+import {useState} from "react";
+import {Alert, Button, Card, Form, InputGroup, Row} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {LOGIN, REGISTER, LOGIN_ERROR} from "../modules/files";
+import {BsLock, BsPerson} from "react-icons/bs";
+
+function Login({_useDispatch = useDispatch, _useSelector = useSelector}) {
+    const dispatch = _useDispatch()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [submit, setSubmit] = useState(false)
+    const successfulRegisterMessage = _useSelector(state => state.successfulRegisterMessage)
+    const loginErrorMessage = _useSelector(state => state.loginErrorMessage)
+    const users = _useSelector(state => state.users)
+
+    function validateCredentials() {
+         const isCredentialsExist =  users.findIndex((user) =>
+             user.username === username &&
+            user.password === password) >= 0;
+
+         if (isCredentialsExist) {
+             dispatch({type: LOGIN, credentials: {username, password}})
+         } else {
+             dispatch({type: LOGIN_ERROR})
+         }
+    }
+
+    function onUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function onPasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setSubmit(true)
+        validateCredentials();
+    }
+
+    return <Card style={{borderWidth: 0}}>
+        <Card.Body>
+            {successfulRegisterMessage ? (<Alert variant={"success"}>
+                    Successfully Registered. Please login
+                </Alert>) : null}
+            {loginErrorMessage ? (<Alert variant={"danger"}>
+                Failed to login
+            </Alert>) : null}
+            <Form onSubmit={handleSubmit}>
+                <InputGroup className="mb-2">
+                    <InputGroup.Text><BsPerson/></InputGroup.Text>
+                    <Form.Control type='text' placeholder="Username" onChange={onUsernameChange}
+                                  isInvalid={submit && !username}/>
+                </InputGroup>
+
+                <InputGroup className="mb-2">
+                    <InputGroup.Text><BsLock/></InputGroup.Text>
+                    <Form.Control type='password' placeholder="Password" onChange={onPasswordChange}
+                                  isInvalid={submit && !password}/>
+                </InputGroup>
+
+                <Row className={'p-3'}>
+                    <Button type={"submit"} variant={"primary"}>Login</Button>
+                </Row>
+                <Row className={'p-3'}>
+                    <Button type={"button"} variant={"primary"} onClick={() => dispatch({type: REGISTER})}>Register</Button>
+                </Row>
+            </Form>
+        </Card.Body>
+    </Card>
+}
+
+export default Login
---
+++ src/components/Login.test.js
@@ -0,0 +1,77 @@
+import { render, screen } from '@testing-library/react';
+import Login from './Login';
+import userEvent from "@testing-library/user-event";
+import reducer, {LOGIN, LOGIN_ERROR, REGISTER} from "../modules/files";
+
+
+it('should show username and password inputs', () => {
+    render(<Login _useDispatch={() => {}} _useSelector={() => {}}/>);
+    const usernameElement = screen.getByPlaceholderText(/Username/);
+    expect(usernameElement).toBeInTheDocument();
+    const passwordElement = screen.getByPlaceholderText(/Password/);
+    expect(passwordElement).toBeInTheDocument();
+});
+
+it('should show login button', () => {
+    render(<Login _useDispatch={() => {}} _useSelector={() => {}}/>);
+    const loginButton = screen.getByText(/Login/);
+    expect(loginButton).toBeInTheDocument();
+});
+
+it('should show register button', () => {
+    render(<Login _useDispatch={() => {}} _useSelector={() => {}}/>);
+    const registerButton = screen.getByText(/Register/);
+    expect(registerButton).toBeInTheDocument();
+});
+
+it('should dispatch LOGIN with typed creds when login button clicked', () => {
+    const dispatch = jest.fn()
+    const credentials = {
+        username: 'user1',
+        password: 'password1'
+    }
+    const users = [
+        {username: 'user1', password: 'password1'},
+        {username: 'user2', password: 'password2'},
+        {username: 'user3', password: 'password3'},
+    ]
+    const state = {
+        users
+    }
+    render(<Login _useDispatch={() => dispatch} _useSelector={fn => fn(state)}/>)
+    userEvent.type(screen.getByPlaceholderText('Username'), credentials.username)
+    userEvent.type(screen.getByPlaceholderText('Password'), credentials.password)
+    userEvent.click(screen.getByText('Login'))
+    expect(dispatch).toHaveBeenCalledWith({type: LOGIN, credentials})
+})
+
+it('should dispatch REGISTER when Register button is clicked', () => {
+    const dispatch = jest.fn()
+    render(<Login _useDispatch={() => dispatch} _useSelector={() => {}}/>)
+    userEvent.click(screen.getByText('Register'))
+    expect(dispatch).toHaveBeenCalledWith({type: REGISTER})
+})
+
+it('should dispatch LOGIN_ERROR when credentials entered are incorrect', () => {
+    const dispatch = jest.fn()
+    const credentials = {
+        username: '',
+        password: ''
+    }
+    const users = [
+        {username: 'user1', password: 'password1'},
+        {username: 'user2', password: 'password2'},
+    ]
+    const currState = {
+        users
+    }
+    render(<Login _useDispatch={() => dispatch} _useSelector={fn => fn(currState)}/>)
+    const usernameElement = screen.getByPlaceholderText(/Username/);
+    expect(usernameElement).toBeInTheDocument();
+    const passwordElement = screen.getByPlaceholderText(/Password/);
+    expect(passwordElement).toBeInTheDocument();
+    //expect(screen.getByPlaceholderText('Username'), credentials.username)
+    //userEvent.type(screen.getByPlaceholderText('Password'), credentials.password)
+    userEvent.click(screen.getByText('Login'))
+    expect(dispatch).toHaveBeenCalledWith({type: LOGIN_ERROR})
+})
