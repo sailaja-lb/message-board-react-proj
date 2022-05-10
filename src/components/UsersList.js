84e71f2ef8ee9248a85d1adb65c1a4d95ecb1ae6
sailaja-lb <lakkakulasailaja@gmail.com>
2022-05-10 09:53:37 -0500

    userslist

---
+++ src/components/UsersList.js
@@ -0,0 +1,31 @@
+import {useDispatch, useSelector} from "react-redux";
+import {Card} from "react-bootstrap";
+import {POST_MESSAGE} from "../modules/files";
+
+export default function UsersList({_useSelector = useSelector, _useDispatch = useDispatch}) {
+    const dispatch = _useDispatch();
+    const loggedInUser = _useSelector(state => state.loggedInUser)
+    const users = _useSelector(state => state.users)
+
+    function createPersonalMessage(event, toUser) {
+        event.preventDefault()
+        const message = window.prompt(`Send message to ${toUser}`)
+        if (message) {
+            dispatch({type: POST_MESSAGE, message: {message, from: loggedInUser, to: toUser}})
+        }
+    }
+    return (
+        <Card>
+            <Card.Header><h5>Users List</h5></Card.Header>
+            <Card.Body>
+                {users.map(user => (loggedInUser !== user.username) ?
+                    <div key={user.username}>
+                        <a href={"#"} onClick={event => createPersonalMessage(event, user.username)}>
+                            {user.username}
+                        </a>
+                    </div>:
+                    null)}
+            </Card.Body>
+        </Card>
+    )
+}
---
+++ src/components/UsersList.test.js
@@ -0,0 +1,38 @@
+import {render, screen} from "@testing-library/react";
+import UsersList from "./UsersList";
+import {POST_MESSAGE} from "../modules/files";
+import userEvent from "@testing-library/user-event";
+
+
+it('should display all the users except the loggedInUser',() => {
+    const loggedInUser = 'user1'
+    const state = {users: [{username: 'user1', password:''}, {username: 'user2', password:''}], loggedInUser}
+
+    render(<UsersList _useDispatch={()=> {}} _useSelector={fn => fn(state)} />)
+    expect(screen.getByText('user2')).toBeInTheDocument()
+    expect(screen.queryByText('user1')).not.toBeInTheDocument()
+})
+it('should generate a window prompt and dispatch POST_MESSAGE when username link is clicked', () => {
+    const dispatch = jest.fn()
+    const message = {
+        from: 'user1',
+        message: 'hi user2',
+        to: 'user2'
+    }
+    window.prompt = jest.fn().mockImplementation(() => 'hi user2')
+    const loggedInUser = 'user1'
+    const state = {users: [{username: 'user1', password:''}, {username: 'user2', password:''}], loggedInUser}
+    render(<UsersList _useDispatch={() => dispatch} _useSelector={fn=> fn(state)} />)
+    const userLink = screen.getByRole('link', { name: 'user2' });
+    userEvent.click(userLink)
+    expect(window.prompt).toHaveBeenCalled()
+    expect(dispatch).toHaveBeenCalledWith({type: POST_MESSAGE, message})
+});
+it('should display Users List heading', ()=> {
+    const dispatch = jest.fn()
+    const header = 'Users List'
+    const loggedInUser = 'user1'
+    const state = {users: [{username: 'user1', password:''}, {username: 'user2', password:''}], loggedInUser}
+    render(<UsersList _useDispatch={()=>dispatch} _useSelector={fn=>fn(state)} />)
+    expect(screen.getByText(header)).toBeInTheDocument()
+})
