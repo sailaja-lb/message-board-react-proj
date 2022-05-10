0f346b231dac6f995929cd86176b1992edb8cf0b
sailaja-lb <lakkakulasailaja@gmail.com>
2022-05-10 11:11:49 -0500

    small corrections

--- src/App.test.js
+++ src/App.test.js
@@ -2,38 +2,36 @@ import { render, screen } from '@testing-library/react';
 import {App} from './App';
 
 
-test('should display _Home when logged in', () => {
+it('should display _Home when isLoggedIn is true', () => {
   const expectedHome = 'This is Homepage'
   const mockHome = () => <div>{expectedHome}</div>
 
   const state = {
     isLoggedIn: true
   }
-  render(<App _Home={mockHome} _useDispatch={() => {}}
-              _useSelector={fn => fn(state)}/>)
+  render(<App _Home={mockHome} _useSelector={fn => fn(state)}/>)
   expect(screen.getByText(expectedHome)).toBeInTheDocument()
 });
 
-test('should display _Register screen when isRegister is true', () => {
+it('should display _Register screen when isRegister is true', () => {
   const expectedRegister = 'This is Register Page'
   const mockRegister = () => <div>{expectedRegister}</div>
 
   const state = {
     isRegister: true
   }
-  render(<App _Register={mockRegister} _useDispatch={() => {}}
-              _useSelector={fn => fn(state)}/>)
+  render(<App _Register={mockRegister} _useSelector={fn => fn(state)}/>)
   expect(screen.getByText(expectedRegister)).toBeInTheDocument()
 });
 
-test('should display the login screen when logged out', () => {
+it('should display the login screen when logged out(isLoggedIn is false)', () => {
   const expectedText = 'This is the expected text'
   const mockLogin = () => <>{expectedText}</>
 
   const state = {
     isLoggedIn: false
   }
-  render(<App _Login={mockLogin} _useDispatch={() => {}} _useSelector={fn => fn(state)}/>)
+  render(<App _Login={mockLogin} _useSelector={fn => fn(state)}/>)
   expect(screen.getByText(expectedText)).toBeInTheDocument()
 });
 
